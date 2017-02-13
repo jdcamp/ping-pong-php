12202474f4f19924720af6a1d5e1bc1ede071532
Jake Campa and Maggie Harrington <maggie@>
2017-02-13 09:55:23 -0800

    add all backend function

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -5,7 +5,18 @@
         function generatePingPongArray($number) {
             $outputArray = array();
             for ($i=1; $i <= $number; $i++) {
-                array_push($outputArray, $i);
+                if ($i % 3 == 0 && $i % 5 != 0) {
+                    array_push($outputArray, "ping");
+                } elseif ($i % 5 == 0) {
+                    if ($i % 3 == 0) {
+                        array_push($outputArray, "ping-pong");
+                    }else {
+                        array_push($outputArray, "pong");
+                    }
+                } else {
+                    array_push($outputArray, $i);
+                }
+
             }
             return implode(" ", $outputArray);
         }
--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -6,11 +6,11 @@ class PingPongGeneratorTest extends PHPUnit_Framework_TestCase
     function test_checkPingPong_false()
     {
         $test_CheckPingPong = new PingPongGenerator;
-        $input = 4;
+        $input = 15;
 
         $result = $test_CheckPingPong->generatePingPongArray($input);
 
-        $this->assertEquals("1 2 3 4", $result);
+        $this->assertEquals("1 2 ping 4 pong ping 7 8 ping pong 11 ping 13 14 ping-pong", $result);
     }
 }
 
